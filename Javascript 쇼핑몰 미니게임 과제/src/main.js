227f455182c03fa085f1f202414568efc44c07c6
river040118 <river040118@gmail.com>
2025-07-02 23:39:48 +0900

    JavaScript - display items

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -4,10 +4,27 @@ function loadItems(){
     .then(response =>response.json())
     .then(json => json.items);
 }
+
+//update the list with the given items
+function displayItems(items){
+    const container = document.querySelector('.items');
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
+
+// 데이터 아이템으로부터 HTML 리스트 작성하기
+function createHTMLString(item){
+    return `
+           <li class="item">
+               <img src="${item.image}" alt="${item.type}" class="item__thumbnail" />
+               <span class="item__description">${item.gender}, ${item.size} </span> 
+            </li>
+        `;
+}
 loadItems()
     .then(items => {
         console.log(items)
-        // displayItems(items);
-        // setEventListeners(items);
+        displayItems(items);
+        setEventListeners(items);
     })
+    
 .catch(console.log);
