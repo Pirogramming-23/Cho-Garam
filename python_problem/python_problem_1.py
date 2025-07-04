873c90414434e63a7e2305ce5ac7d3c82685ff49
river040118 <river040118@gmail.com>
2025-07-05 03:01:32 +0900

    3단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,3 +1,19 @@
 num = 0
 
-num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+### 정수 타입으로만 받으면 그 이외의 상황에 에러가 나므로 2단계에 대해서 수정함
+
+while True:
+    if not num.isdigit(): ###정수 확인 메서드 사용
+        print("정수를 입력하세요")
+        num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+        continue
+
+    num = int(num)
+
+    if num not in [1, 2, 3]:
+        print("1, 2, 3 중 하나를 입력하세요")
+        num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+        continue
+
+    break
