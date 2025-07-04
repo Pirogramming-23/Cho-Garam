1b653f2ad964bbf147d42bab56fd5584e4c7372b
river040118 <river040118@gmail.com>
2025-07-05 03:34:49 +0900

    8단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,14 +1,7 @@
-num = 0
-
-current = 1 ### 초기값 설정
-
-while current <= 31: ### 현재 값이 31이 될 떄 까지 반복
-    
+def brGame(player, current):
     num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
-    ### 정수 타입으로만 받으면 그 이외의 상황에 에러가 나므로 2단계에 대해서 수정함
-
     while True:
-        if not num.isdigit(): ###정수 확인 메서드 사용
+        if not num.isdigit():
             print("정수를 입력하세요")
             num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
             continue
@@ -23,37 +16,20 @@ while current <= 31: ### 현재 값이 31이 될 떄 까지 반복
         break
 
     for i in range(num):
-        print(f"playerA : {current}")
+        print(f"{player} : {current}")
         if current == 31:
-            print("playerB win!") 
+            if player == "playerA":
+                print("playerB win!")
+            else:
+                print("playerA win!")
             exit()
         current += 1
 
-    ### 몇 개 까지 작성했는지 알아야 다음 플레이어를 추가 할 수 있으므로 현재 값 추가
-
-    num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
-
-    ##B 입력 받음
-
-    while True:
-        if not num.isdigit(): ###정수 확인 메서드 사용
-            print("정수를 입력하세요")
-            num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
-            continue
-
-        num = int(num)
-
-        if num not in [1, 2, 3]:
-            print("1, 2, 3 중 하나를 입력하세요")
-            num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
-            continue
-
-        break
+    return current
 
 
-    for i in range(num):
-        print(f"playerB : {current}")
-        if current == 31:
-            print("playerA win!") 
-            exit()
-        current += 1
+
+current = 1
+while True:
+    current = brGame("playerA", current)
+    current = brGame("playerB", current)
