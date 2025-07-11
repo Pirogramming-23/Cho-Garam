c4ebefd8da97b7ea25479afa53fcb23eca0c6466
river040118 <river040118@gmail.com>
2025-07-11 16:29:34 +0900

    [add] 기능 5 구현

--- myMovieReviews/reviews/urls.py
+++ myMovieReviews/reviews/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from reviews.views import * 
 
 urlpatterns = [
-    path('', reviews_list),
+    path('', reviews_list, name='reviews_list'),
     path("<int:pk>/", reviews_read),
     path("create/", reviews_create),
     path("<int:pk>/delete/", reviews_delete),
