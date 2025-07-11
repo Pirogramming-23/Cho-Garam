1c2550435a99e5f0ea7bd5353689615db87df805
river040118 <river040118@gmail.com>
2025-07-11 15:21:41 +0900

    DB 세팅

--- myMovieReviews/reviews/models.py
+++ myMovieReviews/reviews/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+class Review(models.Model):
+    title = models.CharField(max_length=32)
+    release = models.CharField(max_length=32)
+    genre = models.CharField(max_length=32)
+    score = models.CharField(max_length=32)
+    runtime = models.CharField(max_length=32)
+    director = models.CharField(max_length=32)
+    actor = models.CharField(max_length=32)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
 # Create your models here.
